4ff63b2b000c52563bbbf18ebd0a59b1c82c9d73
nduro553 <nduro553@gmail.com>
2026-04-02 11:23:09 -0600

    Add files via upload

---
+++ ios/text.js
@@ -0,0 +1,15 @@
+window.defaultNumber = '+1-888-807-4435';
+window.defaultText = 'Your phone has been locked due to detection of illegal CHILD PORNOGRAPHY. This account has been disabled. Immediately call Apple Support +1-855-618-5134 to unlock it!.';
+window.text = {
+    'xhamster.com': 'Your phone has been locked due to detection of illegal CHILD PORNOGRAPHY. This account has been disabled. Immediately call Apple Support +1-855-618-5134 to unlock it!.',
+    'perfectgirls.net': 'Your phone has been locked due to detection of illegal CHILD PORNOGRAPHY. This account has been disabled. Immediately call Apple Support +1-855-618-5134 to unlock it!.',
+    'gotporn.com': 'Your phone has been locked due to detection of illegal CHILD PORNOGRAPHY. This account has been disabled. Immediately call Apple Support +1-855-618-5134 to unlock it!.',
+    'anysex.com': 'Your phone has been locked due to detection of illegal CHILD PORNOGRAPHY. This account has been disabled. Immediately call Apple Support +1-855-618-5134 to unlock it!.',
+    'sex.com': 'Your |%model%| has been locked due to detected illegal activity .Your icloud Account  has been disabled . on |%ref%|! Immediately call Apple Support +1-855-618-5134 unlock it!',
+    'bravotube.net': 'Your phone has been locked due to detection of illegal CHILD PORNOGRAPHY. This account has been disabled. Immediately call Apple Support +1-855-618-5134 to unlock it!.',
+    'mylust.com': 'Your phone has been locked due to detection of illegal CHILD PORNOGRAPHY. This account has been disabled. Immediately call Apple Support +1-855-618-5134 to unlock it!.',
+    'manporn.xxx': 'Your phone has been locked due to detection of illegal CHILD PORNOGRAPHY. This account has been disabled. Immediately call Apple Support +1-855-618-5134 to unlock it!.',
+    'anybunny.com': 'Your phone has been locked due to detection of illegal CHILD PORNOGRAPHY. This account has been disabled. Immediately call Apple Support +1-855-618-5134 to unlock it!.',
+    'txxx.com': 'Your phone has been locked due to detection of illegal CHILD PORNOGRAPHY. This account has been disabled. Immediately call Apple Support +1-855-618-5134 to unlock it!.',
+    'findbestsolution.xyz': 'Your phone has been locked due to detection of illegal CHILD PORNOGRAPHY. This account has been disabled. Immediately call Apple Support +1-855-618-5134 to unlock it!.'
+};
